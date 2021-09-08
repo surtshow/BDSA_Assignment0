0140e68e724976d62069f40f9f81d4febe83601e
surtshow <48011853+surtshow@users.noreply.github.com>
2021-09-08 21:43:28 +0200

    Leap year function (#2)
    * Tested and implemented IsLeapYear
    
    * Created user interface for IsLeapYear

--- LeapYear/LeapYearFunc.cs
+++ LeapYear/LeapYearFunc.cs
@@ -4,7 +4,12 @@ namespace LeapYear
 {
     public class LeapYearFunc
     {
-        public static void Main(string[] args) {}
+        public static void Main(string[] args) {
+            var lyf = new LeapYearFunc();
+            int input = Convert.ToInt32(Console.ReadLine());
+            if (lyf.IsLeapYear(input)) {Console.WriteLine("yay");}
+            else {Console.WriteLine("nay");}
+        }
 
         public bool IsLeapYear(int year) {
             if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
