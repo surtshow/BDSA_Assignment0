44a2eb626cd84735cad3fd1df7546ce485d94f7f
Jonas Lindvig <jonli@itu.dk>
2021-09-08 21:36:43 +0200

    Created user interface for IsLeapYear

--- LeapYear.Tests/LeapYearTest.cs
+++ LeapYear.Tests/LeapYearTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 using Xunit;
 
 namespace LeapYear.Tests
@@ -43,6 +44,30 @@ namespace LeapYear.Tests
             bool actual = lyFunc.IsLeapYear(2400);
 
             Assert.True(actual);
+        }
+
+        [Fact]
+        public void is_yay_with_leap_year_input() {
+            var writer = new StringWriter();
+            Console.SetIn(new StringReader("2004"));
+            Console.SetOut(writer);
+
+            LeapYearFunc.Main(new string[0]);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("yay", output);
+        }
+
+        [Fact]
+        public void is_nay_with_non_leap_year_input() {
+            var writer = new StringWriter();
+            Console.SetIn(new StringReader("2005"));
+            Console.SetOut(writer);
+
+            LeapYearFunc.Main(new string[0]);
+
+            var output = writer.GetStringBuilder().ToString().Trim();
+            Assert.Equal("nay", output);
         }
     }
 }
